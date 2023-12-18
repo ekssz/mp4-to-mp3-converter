c23e82b1bea00c4882ffd73c903f46bd380358bd
Valentine <81739569+ekssz@users.noreply.github.com>
2023-12-18 15:13:01 +0300

    console version of the program
    The first version of the program works only in the console, so far there is no way to specify the final path for saving sound, both sound and video are saved at once, which can affect the memory on your disk.
    For correct operation, the path in which the files will be stored must be inserted in the code.
    
    download_path = "your path, like d:/music"

---
+++ mp4tomp3console.py
@@ -0,0 +1,21 @@
+import pytube
+import moviepy.editor as mp
+import os
+import time
+
+url = input("Посилання на відео з ютубу: ")
+youtube = pytube.YouTube(url)
+video = youtube.streams.get_highest_resolution()
+
+download_path = "d:/programming/python/pythonconverter"
+video.download(output_path=download_path, filename="temp_video.mp4")
+
+video_path = os.path.join(download_path, "temp_video.mp4")
+while not os.path.exists(video_path):
+    time.sleep(1)
+
+video_clip = mp.VideoFileClip(video_path)
+output_path = os.path.join(download_path, "output_audio.mp3")
+video_clip.audio.write_audiofile(output_path)
+
+print("---end---")
